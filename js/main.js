0f2e960c8902b3599dc7be6f95e2efe2eb56b4e8
Gabriel Gianordoli <gianordoligabriel@gmail.com>
2016-09-24 13:48:55 -0400

    walls done

--- js/main.js
+++ js/main.js
@@ -6,34 +6,42 @@ app.main = (function(){
 
 	function matterSetup(){
 
+		var container = document.getElementById('canvas-container');
+		// console.log(container);
+
+
 		/*------------- MATTER OBJECTS -------------*/
-		// Matter module aliases
+		// ALIASES
 		var Engine = Matter.Engine,
 		    Render = Matter.Render,
 		    World = Matter.World,
 		    Bodies = Matter.Bodies,
 			Events = Matter.Events,
 			MouseConstraint = Matter.MouseConstraint,
-			Mouse = Matter.Mouse
+			// Mouse = Matter.Mouse,
+			Body = Matter.Body
 			;
 
-		/*------------- WORLD SETUP -------------*/
-		//Matter.js canvas
-		var container = document.getElementById('canvas-container');
-		console.log(container);
+		// SETUP WORLD
 
 		// create an engine
 		var engine = Engine.create();
 
 		// create a renderer
+		var width = window.innerWidth;
+		var height = window.innerHeight;
 		var render = Render.create({
 		    element: container,
 		    engine: engine,
 		    options: {
 		    	wireframes: false,
-		    	showAngleIndicator: false
+		    	showAngleIndicator: false,
+		    	width: width,
+		    	height: height,
+		    	background: "black"
 		    }
 		});
+		// console.log(render);
 
 		// add a mouse controlled constraint
         var mouseConstraint = MouseConstraint.create(engine, {
@@ -44,26 +52,33 @@ app.main = (function(){
             	}
             }
         });
-		World.add(engine.world, mouseConstraint);
 		engine.world.gravity.y = 0;
 
 		var boxA = Bodies.rectangle(400, 200, 80, 80, { isStatic: false });
-		var boxB = Bodies.rectangle(450, 50, 80, 80, { isStatic: false });
-		var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+		var boxB = Bodies.rectangle(450, 300, 80, 80, { isStatic: false });
+
+		var wallWidth = 100;
+		var walls = [
+			Bodies.rectangle(-wallWidth, height/2, wallWidth, height, { isStatic: true }),
+			Bodies.rectangle(width+wallWidth, height/2, wallWidth, height, { isStatic: true }),
+			Bodies.rectangle(width/2, -wallWidth, width, wallWidth, { isStatic: true }),
+			Bodies.rectangle(width/2, height+wallWidth, width, wallWidth, { isStatic: true })
+		];
+		// var ground = 
 
 		// add all of the bodies to the world
-		World.add(engine.world, [boxA, boxB, ground]);
+		World.add(engine.world, mouseConstraint);
+		World.add(engine.world, [boxA, boxB]);
+		World.add(engine.world, walls);
 
 		
 		// console.log(mouseConstraint);
 		// console.log(boxA);
 
-Events.on(mouseConstraint, "mousemove", function(event){
-	// mouseConstraint.constraint.pointA = boxA.position;
-	// mouseConstraint.constraint.pointB = boxA.position;
-	console.log('A', mouseConstraint.constraint.pointA);
-	console.log('B', mouseConstraint.constraint.pointB);
-});
+		// FOLLOW MOUSE
+		Events.on(mouseConstraint, "mousemove", function(event){
+			Body.setPosition(boxA, mouseConstraint.constraint.pointA);
+		});
 
 		// run the engine
 		Engine.run(engine);
